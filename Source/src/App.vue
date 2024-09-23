9d2cc65e57eecc3949cc451c53ab53850fb5fb81
nguyenphat006 <nguyendangphat1505@gmail.com>
2024-09-23 15:05:25 +0700

    Phát commit

---
+++ Source/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+// import Bai1 from './components/Bai3.vue'
+
+import { ref } from 'vue'
+import CreatePost from './components/CreatePost.vue'
+import PostList from './components/PostList.vue'
+
+  const posts = ref([])
+  function addPost(post){
+    posts.value.push(post)
+  }
+</script>
+
+<template>
+ <!-- <Bai1></Bai1> -->
+  <div class="app">
+    <h1>Ứng dụng Blog nhỏ với vue.js</h1>
+    <CreatePost @add-post="addPost"/>
+    <PostList :posts="posts"/>
+  </div>
+</template>
+
+<style>
+  #app{
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+  }
+</style>
---
+++ Source/src/components/Bai3.vue
@@ -0,0 +1,36 @@
+<template>
+   <div class="row">
+     <div class="col-sm-12">
+        <h1>Thông tin cá nhân</h1>
+        <form>
+            <div class="mb-3">
+                <label>Họ và tên:</label>
+                <input v-model="userInfo.name" type="text" class="form-control" placeholder="Nhập họ và tên">
+            </div>
+            <div class="mb-3">
+                <label>Tuổi:</label>
+                <input v-model="userInfo.age" type="text" class="form-control" placeholder="Nhập tuổi">
+            </div>
+            <div class="mb-3">
+                <label>Email:</label>
+                <input v-model="userInfo.email" type="text" class="form-control" placeholder="Nhập email">
+            </div>  
+        </form>
+        <div class="info-display mt-4 p-3 bg-dark text-white rounded">
+            <h2>Thông tin đã nhập:</h2>
+            <p><strong>Họ và tên:</strong> {{ userInfo.name }}</p>
+            <p><strong>Tuổi:</strong> {{ userInfo.age }}</p>
+            <p><strong>Email:</strong> {{ userInfo.email }}</p>
+        </div>
+    </div>
+   </div>
+</template>
+<script setup>
+import { reactive } from 'vue'
+
+const userInfo = reactive({
+    name: '',
+    age: null,
+    email: ''
+});
+</script>
